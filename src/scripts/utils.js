4bd773b123001cdc85b10dcc6da03f2ea11f984e
Xherdi Lika <dscherdil@gmail.com>
2020-06-20 22:54:26 +0200

    swipe motion bug fix

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -24,36 +24,35 @@
     allowedTime = 300, // maximum time allowed to travel that distance
     elapsedTime,
     startTime,
-    handleswipe = callback || function(swipedir){}
+    handleswipe = callback || function(swipedir){};
   
     touchsurface.addEventListener('touchstart', function(e){
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
+        e.preventDefault();
+        var touchobj = e.changedTouches[0];
+        swipedir = 'none';
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
     }, false)
   
     touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
+        e.preventDefault(); // prevent scrolling when inside DIV
     }, false)
   
     touchsurface.addEventListener('touchend', function(e){
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        e.preventDefault();
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
         if (elapsedTime <= allowedTime){ // first condition for awipe met
             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'leftSwipe' : 'rightSwipe' // if dist traveled is negative, it indicates left swipe
+                swipedir = (distX < 0)? 'leftSwipe' : 'rightSwipe'; // if dist traveled is negative, it indicates left swipe
             }
             else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'upSwipe' : 'downSwipe' // if dist traveled is negative, it indicates up swipe
+                swipedir = (distY < 0)? 'upSwipe' : 'downSwipe'; // if dist traveled is negative, it indicates up swipe
             }
         }
-        handleswipe(swipedir)
-        e.preventDefault()
+        handleswipe(swipedir);
     }, false)
 } 
